29d3fa5c0a76e3b7758ae8f44bc801ca37f12608
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-27 21:22:21 +0200

    command out home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import store from './store'
 import {Provider} from 'react-redux'
 import { Route } from 'react-router-dom'
-import Home from './components/Home'
+//import Home from './components/Home'
 import EventListContainer from './components/EventListContainer'
 
 class App extends Component {
