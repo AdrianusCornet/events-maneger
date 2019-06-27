7ce0bcbdbbfdbca47902ed0df33018c78444f2b9
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-27 16:50:53 +0200

    make event reduser

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,1 +1,12 @@
-import { EVENTS_FETCHED, loadEvents } from '../actions/events'
+import { EVENTS_FETCHED, loadEvents } from '../actions/events';
+
+const initialState = null
+
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+    case EVENTS_FETCHED:
+      return action.events
+    default:
+      return state
+  }
+}
