fee7d2b7ede0a2e082be30cde2d0d41f88feaaa5
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-27 18:11:15 +0200

    event list

---
+++ src/components/EventList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default class EventList extends React.Component {
+  renderEvents = () => {
+    if (!this.props.events) {
+      return <span>loading...</span>
+    }
+    return this.props.events.map(this.renderEvent)
+  }
+  renderEvent = (event) => {
+    return <li><Link to={`/events/${event.id}`}>{event.description}</Link></li>
+  }
+  render() {
+    return (
+      <ul>
+        {this.renderEvents()}
+      </ul>
+    )
+  }
+}
