2bd924df05259781cf5a2a49dd4241980fc88955
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-28 11:09:32 +0200

    create input

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -3,15 +3,32 @@ import React, { Component } from 'react'
 export default class EventForm extends Component {
   render() {
     return (
-      <form>
-        name:
-        <input type='text' name='name' />
-        <br />
-        date:
-        <input type='text' name='date' />
-        <br />
-        description:
-        <input type='text' name='description' />
+      <form onSubmit={this.props.onSubmit}>
+        <label>
+          Name:
+          <input
+            type='text'
+            name='name'
+            onChange={this.props.onChange} />
+        </label>
+        <br/>
+        <label>
+          date:
+          <input
+            type='date'
+            name='date'
+            onChange={this.props.onChange} />
+        </label>
+        <br/>
+        <label>
+          description:
+          <input
+            type='text'
+            name='description'
+            onChange={this.props.onChange} />
+        </label>
+        <br/>
+        <input type='submit' value='Submit' />
       </form>
     )
   }
